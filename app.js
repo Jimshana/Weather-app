ffc56738e4b0294bbeac28078797f68cabcf61a5
Jimshana <jimshana2193@gmail.com>
2021-09-29 14:18:22 +0530

    final commit

--- app.js
+++ app.js
@@ -12,10 +12,32 @@ inputBox.onkeyup = ()=>{
   }
 }
 
+inputBox.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+      event.preventDefault();
+      let userEnteredValue = {"data":inputBox.value,"checked":false};
+      if(inputBox.value==""){
+       return false;
+      }
+      let getLocalStorageData = localStorage.getItem("New Todo"); 
+      if(getLocalStorageData == null){
+        listArray = []; 
+      }else{
+        listArray = JSON.parse(getLocalStorageData); 
+      }
+    
+      listArray.push(userEnteredValue); 
+      localStorage.setItem("New Todo", JSON.stringify(listArray)); 
+      showTasks();
+      addBtn.classList.remove("active"); 
+     
+  }
+});
+
 showTasks();
 
 addBtn.onclick = ()=>{
-    let userEnteredValue = inputBox.value; 
+  let userEnteredValue = {"data":inputBox.value,"checked":false};
     if(inputBox.value==""){
      return false;
     }
@@ -38,8 +60,7 @@ addBtn.onclick = ()=>{
     }else{
       listArray = JSON.parse(getLocalStorageData); 
     }
-    const pendingTasksNumb = document.querySelector(".pendingTasks");
-    pendingTasksNumb.textContent = listArray.length;
+   
     if(listArray.length > 0){
       deleteAllBtn.classList.add("active");
     }else{
@@ -48,10 +69,16 @@ addBtn.onclick = ()=>{
     let newLiTag = "";
     
     listArray.forEach((element, index) => {
-      newLiTag += `<li id=${index}>  <input type="checkbox" id="checkbox" class="custom_checkbox" onclick="todoChecked(this,${index})"  /> ${element}<span class="icon" onclick="deleteTask(${index})"><i class="fas fa-trash"></i></span></li>`;    });
+      style = element.checked ? "text-decoration:line-through" : "text-decoration:none";
+      checked = element.checked ? "checked" : ""
+      newLiTag += `<li id=${index} style=${style}>  <input type="checkbox" ${checked} id="checkbox" class="custom_checkbox" onclick="todoChecked(this,${index})"  /> ${element.data}<span class="icon" onclick="deleteTask(${index})"><i class="fas fa-trash"></i></span></li>`;
+  
+      
+    });
     todoList.innerHTML = newLiTag;
-    inputBox.value = "";
+    inputBox.value = ""; 
   }
+  
   
  
 function deleteTask(index){
@@ -72,14 +99,21 @@ deleteAllBtn.onclick = ()=>{
 function todoChecked(checkBox,index){
 
   var text = document.getElementById(index);
+  let getLocalStorageData = localStorage.getItem("New Todo");
+  listArray = JSON.parse(getLocalStorageData)
  
   if (checkBox.checked){
+   
+    let check = true;
+    listArray[index] = {data:listArray[index].data,checked:check}
+    localStorage.setItem("New Todo", JSON.stringify(listArray));
     text.style.textDecoration = "line-through"
-  
   } else {
+    let check = false;
+    listArray[index] = {data:listArray[index].data,checked:check}
+    localStorage.setItem("New Todo", JSON.stringify(listArray));
     text.style.textDecoration = 'none'
     
   }
-
 
 }
